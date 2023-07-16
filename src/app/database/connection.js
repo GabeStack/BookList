fb9262c213d088481e8ee73957a006f4ab99b229
Gabriel Barreto <88603031+Cyacer@users.noreply.github.com>
2023-07-15 23:15:49 -0300

    criação do arquivo .process.env

--- src/app/database/connection.js
+++ src/app/database/connection.js
@@ -1,11 +1,12 @@
 import mysql from "mysql2";
-
+import dotenv  from "dotenv";
+dotenv.config();
 const Connection = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "root",
-  database: "BookList",
+  host: process.env.HOST,
+  port: process.env.PORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 Connection.connect();
 /**
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js"
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.SERVERPORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
